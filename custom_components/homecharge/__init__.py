cebd8b113cdfad25dbf3bb8f1750c41a7d150246
Chris Young <chris@unsatisfactorysoftware.co.uk>
2024-02-27 13:46:41 +0000

    Add config schema

--- custom_components/homecharge/__init__.py
+++ custom_components/homecharge/__init__.py
@@ -1,9 +1,20 @@
 from . import homecharge
 
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+
 DOMAIN = "homecharge"
 
 CONF_USER = "username"
 CONF_PASS = "password"
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_USER): cv.string,
+        vol.Required(CONF_PASS): cv.string,
+    })
+}, extra=vol.ALLOW_EXTRA)
+
 
 def setup(hass, config):
 	global hc
